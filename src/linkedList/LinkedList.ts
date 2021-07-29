022d3eb9ffbbd45107be9908aa93918aae9b4c31
Evgeniy Logvinov <evgeniy.logvinov.k@gmail.com>
2021-07-29 13:05:00 +0200

    Linked list reverse

---
+++ src/linkedList/LinkedList.ts
@@ -0,0 +1,86 @@
+// 1. List contains nodes. Each node contains link to the next node.
+import LinkedListNode from "./LinkedListNode";
+export default class LinkedList<T> {
+  head: LinkedListNode<T> = null;
+
+  constructor(head: LinkedListNode<T> = null) {
+    this.head = head;
+  }
+
+  // We need to find the last element and after that add new one and change link
+  appendElement(value: T): void {
+    const newNode = new LinkedListNode<T>(value);
+    if (!this.head) {
+      this.head = newNode;
+      return;
+    }
+
+    let current = this.head;
+
+    while (current.next) {
+      current = current.next;
+    }
+
+    current.next = newNode;
+  }
+
+  // In the prepend method we need to add element
+  // on the start position and after that change
+  // link to our head
+  prependElement(value: T): void {
+    const newNode = new LinkedListNode<T>(value);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      const currentHead = this.head;
+      this.head = newNode;
+      newNode.next = currentHead;
+    }
+  }
+
+  // Firstly we need to check is it first element in the list
+  // or not. If yes, we need to remove only head.
+  // If this is not first element, we need to change links in the previous node
+  deleteElement(index: number): void {
+    if (!index) {
+      this.head = this.head.next;
+
+      return;
+    }
+
+    let previous = this.head;
+
+    for(let i = 0; previous && previous.next; i++) {
+      if (i + 1 === index) {
+        previous.next = previous.next.next;
+        return;
+      }
+      previous = previous.next;
+    }
+  }
+
+  toString(): string {
+    let current = this.head;
+    const res: string[] = [];
+    while(current) {
+      res.push(String(current.value));
+      current = current.next;
+    }
+
+    return res.join(', ');
+  }
+
+  reverse(): void {
+    let current = this.head;
+    let previous: LinkedListNode<T> = null;
+    let next: LinkedListNode<T> = null;
+
+    while (current) {
+      next = current.next;
+      current.next = previous;
+      previous = current;
+      current = next;
+    }
+    this.head = previous;
+  }
+}
--- src/linkedList/linkedList.test.ts
+++ src/linkedList/linkedList.test.ts
@@ -1,6 +1,7 @@
 import LinkedListNode from './LinkedListNode';
+import LinkedList from './LinkedList';
 
-describe('Node tests', () => {
+describe('List Node tests', () => {
   it('should create correct node with value', () => {
     const linkedListNode = new LinkedListNode(14);
     expect(linkedListNode.value).toBe(14);
@@ -22,4 +23,142 @@ describe('Node tests', () => {
     expect(linkedListNode.next).toBeNull();
     expect(linkedListNodeHead.next.value).toBe(2);
   });
+});
+
+describe('List tests', () => {
+  it('should init list without head', () => {
+    const linkedList = new LinkedList();
+    expect(linkedList.head).toBeNull();
+  });
+
+  it('should init list with head', () => {
+    const linkedListNodeHead = new LinkedListNode(120);
+    const linkedList = new LinkedList(linkedListNodeHead);
+    expect(linkedList.head).toBeDefined();
+    expect(linkedList.head.value).toBe(120);
+  })
+
+  describe('prepend', () => {
+    it('should add one item to the linked list', () => {
+      const linkedList = new LinkedList();
+      linkedList.prependElement(120);
+      expect(linkedList.head).toBeDefined();
+      expect(linkedList.head.value).toBe(120);
+    });
+
+    it('should add one more item to the linked list', () => {
+      const linkedList = new LinkedList();
+      linkedList.prependElement(120);
+      linkedList.prependElement(114);
+      expect(linkedList.head).toBeDefined();
+      expect(linkedList.head.value).toBe(114);
+      expect(linkedList.head.next.value).toBe(120);
+    });
+  })
+
+  describe('append', () => {
+    it('should add one item to the linked list', () => {
+      const linkedList = new LinkedList();
+      linkedList.appendElement(120);
+      expect(linkedList.head).toBeDefined();
+      expect(linkedList.head.value).toBe(120);
+    });
+
+    it('should add one more item to the linked list', () => {
+      const linkedList = new LinkedList();
+      linkedList.appendElement(120);
+      linkedList.appendElement(114);
+      linkedList.appendElement(300);
+      expect(linkedList.head.next).toBeDefined();
+      expect(linkedList.head.next.value).toBe(114);
+      expect(linkedList.head.next.next).toBeDefined();
+      expect(linkedList.head.next.next.value).toBe(300);
+    });
+  })
+
+  describe('delete', () => {
+    it('should delete only head when head exists', () => {
+      const linkedList = new LinkedList();
+
+      linkedList.appendElement(11);
+
+      linkedList.deleteElement(0);
+
+      expect(linkedList.toString()).toBe('');
+    });
+
+    it('should delete only one element inside', () => {
+      const linkedList = new LinkedList();
+
+      linkedList.appendElement(11);
+      linkedList.appendElement(22);
+      linkedList.appendElement(33);
+      linkedList.appendElement(44);
+      linkedList.appendElement(55);
+
+      linkedList.deleteElement(3);
+
+      expect(linkedList.toString()).toBe('11, 22, 33, 55');
+    });
+
+    it('should delete the last element', () => {
+      const linkedList = new LinkedList();
+
+      linkedList.appendElement(11);
+      linkedList.appendElement(22);
+      linkedList.appendElement(33);
+      linkedList.appendElement(44);
+      linkedList.appendElement(55);
+
+      linkedList.deleteElement(4);
+
+      expect(linkedList.toString()).toBe('11, 22, 33, 44');
+    });
+
+    it('should not delete element which out of scope', () => {
+      const linkedList = new LinkedList();
+
+      linkedList.appendElement(11);
+      linkedList.appendElement(22);
+      linkedList.appendElement(33);
+      linkedList.appendElement(44);
+      linkedList.appendElement(55);
+
+      linkedList.deleteElement(8);
+
+      expect(linkedList.toString()).toBe('11, 22, 33, 44, 55');
+    });
+  })
+
+  describe('toString', () => {
+    it('should show empty list', () => {
+      const linkedList = new LinkedList();
+      expect(linkedList.toString()).toBe('');
+    });
+
+    it('should show two elements', () => {
+      const linkedList = new LinkedList();
+      linkedList.appendElement(120);
+      linkedList.appendElement(114);
+      expect(linkedList.head).toBeDefined();
+      expect(linkedList.head.value).toBe(120);
+      expect(linkedList.head.next).toBeDefined();
+      expect(linkedList.head.next.value).toBe(114);
+      expect(linkedList.toString()).toBe('120, 114');
+    });
+  })
+
+  describe('reverse', () => {
+    it('should change list order', () => {
+      const linkedList = new LinkedList();
+      linkedList.appendElement(120);
+      linkedList.appendElement(130);
+      linkedList.appendElement(140);
+      linkedList.appendElement(144);
+      linkedList.appendElement(164);
+      linkedList.reverse();
+
+      expect(linkedList.toString()).toBe('164, 144, 140, 130, 120');
+    });
+  })
 });
